7ecba7bb658bcf5601453b540812e202f31fe248
mark <markpackham1@gmail.com>
2021-12-11 20:09:14 +0000

    add migration
    https://youtu.be/aq2E1oaksag?t=564

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::post('/subscribe', [NewsletterController::class, 'subscribe']);
